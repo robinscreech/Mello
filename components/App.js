b1153f60ceb9a318b1311bc82d097031644076db
= <robinscreech4@gmail.com>
2017-11-29 18:14:40 +0000

    Adding start of editing state

--- components/App.js
+++ components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Meta from './Meta';
+import ReactDOM from 'react-dom';
 
 export default class App extends React.Component {
     constructor(props){
@@ -7,6 +8,7 @@ export default class App extends React.Component {
         this.state = {
             data: []
         }
+        this.handleEditCard = this.handleEditCard.bind(this)
     };//end constructor
 
     componentDidMount(){
@@ -18,16 +20,33 @@ export default class App extends React.Component {
                     this.setState({data:jsonRes})
                 })
             })
+        ReactDOM.findDOMNode(this).addEventListener("click", this.handleEditCard);            
+    }
+    
+    componentWillUnmount() {
+        ReactDOM.findDOMNode(this).removeEventListener("click", this.handleEditCard);
+    }
+
+    handleEditCard(event){
+        let i = 0;
+        let cardElement = event.target.parentNode;
+        while((cardElement = cardElement.previousSibling) != null)
+        i++;
+        const items = this.state.data;
+        let inputValue = this.state.data[i].content;
+        items[i].content = <textarea defaultValue={inputValue}></textarea>
+        this.setState({items})
     }
 
   render() {
         const data = this.state.data;
-        const cards = data.map((d) => <div class="card" key={d._id}><div class="card_title">{d.title}</div><div class="card_content">{d.content}</div></div>)
+        const cards = data.map((d) => <div className="card" id={d._id} key={d._id}><div className="card_title">{d.title}</div><div className="card_content">{d.content}</div><div className='card_edit'>edit</div></div>)
 
         return (
-        <div class="container"> 
+        <div className="container"> 
             {cards}
         </div>
     )
   }
 }
+
